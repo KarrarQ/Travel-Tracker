5281295854154d40750f570aaa7669111523670a
Karrar Qasim <Karawr1996@gmail.com>
2023-01-11 18:22:44 -0700

    Fetched data from api

---
+++ src/Api-Calls.js
@@ -0,0 +1,20 @@
+const getData = (dataUrl, objName) => {
+    const retrievedData = fetch(dataUrl)
+      .then(response => response.json())
+      .then(data => {
+        return data[objName] || data;
+      })
+      .catch(error => console.log(error));
+    return retrievedData;
+  }
+  
+  const getAllData = () => {
+    const getAllTravelersData = getData('http://localhost:3001/api/v1/travelers', 'travelers');
+    const getSingleTravelerData = getData('http://localhost:3001/api/v1/travelers/1');
+    const getAllTripsData = getData('http://localhost:3001/api/v1/trips', 'trips');
+    const getAllDestinationsData = getData('http://localhost:3001/api/v1/destinations', 'destinations');
+    const allPromise = Promise.all([getAllTravelersData,getSingleTravelerData, getAllTripsData, getAllDestinationsData]).then(data => data);
+    return allPromise;
+  }
+  
+  export { getAllData }
