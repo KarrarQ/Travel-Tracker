856c25b91d15f367b1291ce6d19a71747ec29794
Karrar Qasim <Karawr1996@gmail.com>
2023-01-13 21:42:11 -0700

    Moved to a new file

---
+++ src/Dom-Updates.js
@@ -0,0 +1,105 @@
+const tripCardsContainer = document.getElementById('tripCardsContainer');
+const userGreeting = document.getElementById('userGreeting');
+const totalSpent = document.getElementById('totalSpent');
+const destinationDropdown = document.getElementById('destinationDropdown');
+const startDateInput = document.getElementById('startDate');
+const tripDurationIput = document.getElementById('tripDuration');
+const numOfTravelersInput = document.getElementById('numberOfTravelers');
+const errorMessage = document.getElementById('errorMessage');
+const userInputForm = document.getElementById('userInputForm');
+
+
+let domUpdates = {
+  renderTravelerTrips(traveler) {
+    tripCardsContainer.innerHTML = '';
+    traveler.trips.forEach(trip => {
+      tripCardsContainer.innerHTML += `
+      <article class="trip-card">
+      <section class="destination-image-container">
+        <img class="destination-image"src="${trip.destination.image}" alt="${trip.destination.alt}">
+      </section>
+      <section class="trip-info">
+        <h4>${trip.destination.destination}</h4>
+        <p>Date: ${trip.date}</p>
+        <p>Travelers: ${trip.travelers}</p>
+        <p>Duration: ${trip.duration}</p>
+        <p>Cost: $${trip.calculateTripCost()}</p>
+        <p>Status: ${trip.status}</p>
+      </section>
+    </article>`
+    });
+  },
+
+  greetUser(traveler) {
+    const names = traveler.name.split(' ');
+    const firstName = names[0];
+    userGreeting.innerText = `Welcome ${firstName}!`;
+  },
+
+  displayAmountSpentYearly(traveler) {
+    totalSpent.innerText = `Total Amount Spent This Year: $${traveler.calculateTotalSpent()}`;
+  },
+
+  addDestinationOptionsToDropdown(destinations) {
+    destinations.forEach(destination => {
+      destinationDropdown.add(new Option(destination.destination, destination.destination));
+    });
+  },
+
+  checkInputValidation() {
+    if (!destinationDropdown.value || !tripDurationIput.value || !numOfTravelersInput.value || !startDateInput.value) {
+      errorMessage.classList.remove('hidden');
+      setTimeout(() => {
+        this.hideResponse(errorMessage, userInputForm)
+      }, 2000);
+    } else if (!this.validateDate()) {
+      errorMessage.innerText = 'Please Pick a Valid Date!'
+      errorMessage.classList.remove('hidden');
+      setTimeout(() => {
+        this.hideResponse(errorMessage, userInputForm)
+      }, 2000);
+    } else if (!this.validateDestination()) {
+      errorMessage.innerText = 'Please Pick a Destination'
+      errorMessage.classList.remove('hidden');
+      setTimeout(() => {
+        this.hideResponse(errorMessage, userInputForm)
+      }, 2000);
+    } else {
+      return true;
+    }
+  },
+
+  validateDestination() {
+    if (destinationDropdown.value !== '--Destination--') {
+      return true;
+    }
+  },
+
+  validateDate() {
+    const todayDate = this.getTodaydate();
+    const correctDate = this.fixInputDate();
+    return correctDate >= todayDate;
+  },
+
+  getTodaydate() {
+    const date = new Date().toISOString().slice(0, 10);
+    const listOfDate = date.split('-');
+    const correctionDate = (listOfDate[2] - 1).toString();
+    listOfDate.splice(2, 1, correctionDate);
+    const todayDate = listOfDate.join('/');
+    return todayDate;
+  },
+
+  fixInputDate() {
+    const initialDate = startDateInput.value.split('-');
+    const correctDate = initialDate.join('/');
+    return correctDate;
+  },
+
+  hideResponse(elem, form) {
+    elem.classList.add('hidden');
+    form.reset();
+  }
+}
+
+export default domUpdates;
