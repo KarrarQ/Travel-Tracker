8c793cf7593da82712f9bd853b524a074b779cc7
Karrar Qasim <Karawr1996@gmail.com>
2023-01-16 23:18:26 -0700

    Added login functionality

--- src/scripts.js
+++ src/scripts.js
@@ -8,7 +8,14 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-
+const loginForm = document.getElementById('loginForm');
+const userNameInput = document.getElementById('userName');
+const passwordInput = document.getElementById('password');
+const loginButton = document.getElementById('loginButton');
+const loginErrorMessage = document.getElementById('loginErrorMessage');
+const loginTitle = document.getElementById('loginTitle');
+const tripInputContainer = document.getElementById('tripInputContainer');
+const tripSelectorContainer = document.getElementById('tripSelectorContainer');
 const searchButton = document.getElementById('searchButton');
 const acceptButton = document.getElementById('acceptButton');
 const cancelButton = document.getElementById('cancelButton');
@@ -23,6 +30,7 @@ let allData;
 
 
 window.addEventListener('load', displayData);
+loginButton.addEventListener('click', checkLoginInfo);
 searchButton.addEventListener('click', checkForm);
 acceptButton.addEventListener('click', acceptTripRequest);
 cancelButton.addEventListener('click', renderForm);
@@ -30,27 +38,76 @@ backButton.addEventListener('click', renderForm);
 pendingTripsButton.addEventListener('click', displayPendingTrips);
 allTripsButton.addEventListener('click', displayAllTrips);
 
+function checkLoginInfo(event) {
+    event.preventDefault();
+    displayData();
+    const id = parseInt(userNameInput.value.slice(8));
+    if (checkLoginInputsAreFilled() && validateUserName() && validatePassword()) {
+      createTraveler(id);
+      domUpdates.hide(loginTitle);
+      domUpdates.hide(loginForm);
+      domUpdates.display(tripInputContainer);
+      domUpdates.display(tripSelectorContainer);
+      intializeData();
+    }
+  }
+
+  const createTraveler = (id) => {
+    currentTraveler = new Traveler(allData[0][id - 1], allData[2], allData[3]);
+    console.log(currentTraveler)
+  }
+  
+  const checkLoginInputsAreFilled = () => {
+    if (!userNameInput.value || !passwordInput) {
+      loginErrorMessage.innerText = 'Please Fill Out All Sections!';
+      domUpdates.display(loginErrorMessage);
+      setTimeout(() => {
+        domUpdates.hideResponse(loginErrorMessage, loginForm);
+      }, 2000);
+    } else {
+      return true;
+    }
+  }
+  
+  const validateUserName = () => {
+    if (userNameInput.value.length < 9 || userNameInput.value.length > 10 || !userNameInput.value.includes('traveler')) {
+      loginErrorMessage.innerText = 'Invalid Username!';
+      domUpdates.display(loginErrorMessage);
+      setTimeout(() => {
+        domUpdates.hideResponse(loginErrorMessage, loginForm);
+      }, 2000);
+    } else {
+      return true
+    }
+  }
+  
+  const validatePassword = () => {
+    if (passwordInput.value !== 'traveler') {
+      loginErrorMessage.innerText = 'The Password Was Wrong. Please Try Again!';
+      domUpdates.display(loginErrorMessage);
+      setTimeout(() => {
+        domUpdates.hideResponse(loginErrorMessage, loginForm);
+      }, 2000);
+    } else {
+      return true
+    }
+  }
 
 function displayData () {
-    const randomUserNum = Math.floor(Math.random() * 50);
-    console.log(randomUserNum)
     getAllData()
       .then(data => {
         allData = data;
         console.log('alldata', allData)
-        intializeData(data, randomUserNum);
       });
   }
 
-  const intializeData = (data, randomId) => {
-    const traveler = new Traveler(data[0][randomId], data[2], data[3]);
-    currentTraveler = traveler
-    console.log('currentT',currentTraveler)
-    domUpdates.renderTravelerTrips(traveler.trips);
-    domUpdates.greetUser(traveler);
-    domUpdates.displayAmountSpentYearly(traveler);
-    domUpdates.addDestinationOptionsToDropdown(data[3])
+  const intializeData = () => {
+    domUpdates.renderTravelerTrips(currentTraveler.trips);
+    domUpdates.greetUser(currentTraveler);
+    domUpdates.displayAmountSpentYearly(currentTraveler);
+    domUpdates.addDestinationOptionsToDropdown(allData[3])
   }
+  
 
   function checkForm(event) {
     event.preventDefault();
